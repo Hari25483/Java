0dd4fb6601cf95dbcc219ea2a0ddd43b90969418
Hari25483 <haricodmw@gmail.com>
2022-01-01 01:45:01 +0530

    Create TestAuthor.java

---
+++ OOP Inheritance & Polymorphism/TestAuthor.java
@@ -0,0 +1,10 @@
+public  class TestAuthor {
+    public static void main(String[] args){
+        Author Hari=new Author("Hari","dadada@gmail.com",'M');
+        System.out.println(Hari);
+
+        Hari.setEmail("sdasda dadad ");
+        System.out.println(Hari);
+
+    }
+}
