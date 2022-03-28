efa126d630917f93de9193ce34725368e22b80e0
Hari25483 <haricodmw@gmail.com>
2022-03-29 00:13:04 +0530

    Update matrixadder.java

--- Threads/matrixadder.java
+++ Threads/matrixadder.java
@@ -1,8 +1,6 @@
 public class matrixadder extends Thread {
     int row_start;
     int row_end;
-//    static int [][] matrix1;
-//    static int [][] matrix2;
     static int[][] matrix1= {{1, 2, 3, 4,0, 0,0, 1},{0, 0,0, 1,0, 0,0, 1},{1, 2, 3, 4,0, 0,0, 1},{1, 2, 3, 4,0, 0,0, 1}};
     static int[][] matrix2= {{1, 2, 3, 4,0, 0,0, 1},{0, 0,0, 1,0, 0,0, 1},{1, 2, 3, 4,0, 0,0, 1},{1, 2, 3, 4,0, 0,0, 1}};
     static int [][] matrix3=new int[matrix1.length][matrix1[0].length];
@@ -18,12 +16,6 @@ public class matrixadder extends Thread {
         this.matrix3=matrix3;
     }
     public static void main(String[] args) throws InterruptedException {
-
-//        System.out.println(matrix1.length);
-//        for (int i=1;i<2;i++){
-//            for (int j=0;j<matrix[0].length;j++){
-//                System.out.println(matrix[i][j]);
-//            }
         adder(matrix1.length,matrix1,matrix2);
         for (int i = 0; i < matrix3.length; i++) {
             for (int j = 0; j < matrix3[0].length; j++) {
@@ -36,8 +28,6 @@ public class matrixadder extends Thread {
 
 
     public static void adder(int row_len, int [][] matrix1, int [][] matrix2) throws InterruptedException{
-
-
         //28/4=7
         int count=row_len/4;
 
@@ -54,16 +44,7 @@ public class matrixadder extends Thread {
         for (int i=row_start;i<row_end;i++){
             for (int j=0;j<column_len;j++){
                 matrix3[i][j]=matrix1[i][j]+matrix2[i][j];
-//                System.out.println(matrix3[i][j]);
-//            if (matrix1[i][j]==matrix2[i][j]){
-//                    System.out.println("same");
-//                    flag=true;
-//                }
-//                else if (matrix1[i][j]!=matrix2[i][j]){
-//                    System.out.println("not same");
-//                    flag=false;
-//                    break;
-//                }
+
             }
         }
 
